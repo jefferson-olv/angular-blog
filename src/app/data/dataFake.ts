967b07312b82d89dde7b9c54a3f80716f9de3943
jeffolv <jefferson.olv@outlook.com>
2023-06-06 23:58:51 -0300

    criada pasta content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+  {
+    "id": "0",
+    "photo": "https://sm.ign.com/ign_br/screenshot/default/5e42b5d3-e57e-4aa4-a020-5e5a1ac8b3b2_bzpm.jpg",
+    "title": "As melhores séries e filmes da HBO Max lançadas em 2023",
+    "description": "Melhor catálogo de séries e filmes."
+  },
+
+  {
+    "id": "1",
+    "photo": "https://pipocasclub.com.br/wp-content/uploads/2022/03/hbo-max-br.jpg",
+    "title": "HBO Max apresenta OS NOVOS LANÇAMENTOS que chegarão na plataforma em 2023; vem conhecer tudo!",
+    "description": "O que chega no mês de Junho no HBO Max."
+  },
+
+  {
+    "id": "2",
+    "photo": "https://www.coxinhanerd.com.br/wp-content/uploads/2023/02/HBOs-The-Last-Of-Us-Episode-4-Episode-5-Arrives-This-Week-1200x675-1.jpg",
+    "title": "The Last Of Us",
+    "description": "A história de survival horror se passa em um mundo pós-apocalíptico. O fungo cordyceps infecta pessoas e criaturas e as torna “zumbis”. O desenrolar acontece num futuro distópico que começa com o surto da doença ocorrendo em 2013, mas o jogo em si se passa vinte anos depois, em 2033."
+  },
+
+  {
+    "id": "3",
+    "photo": "https://www.omegascopio.com.br/wp-content/uploads/2020/07/LOVECRAFT_COUNTRY.jpg",
+    "title": "Lovecraft Country",
+    "description": "Uma nova e empolgante série dramática da HBO que segue a busca de Atticus Freeman pelo pai desaparecido – uma jornada que o leva a ele, à sua amiga Letitia e ao seu tio George numa viagem transcendental pela América de Jim Crow dos anos 50."
+  }
+]
